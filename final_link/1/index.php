6eaa9bf0bd83d2d4caeb7d4e7a497901cda3223d
Terry <tgaitan@csumb.edu>
2018-05-15 15:48:38 +0000

    final

---
+++ final_link/1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FINAL TGAITAN </title>
+    </head>
+    <body>
+     This is my last final!
+     
+     <div> <a href="/">BACK</a></div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <div>
+            <h1>FINAL_3</h1>
+            <a href="/final_link/1">Final_3</a>
+        </div>
+
+    </body>
+</html>
